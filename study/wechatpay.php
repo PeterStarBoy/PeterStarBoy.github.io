5cd1e92ab86f2b5e82e80d39d153b2dddde35a72
PeterStarBoy <1574815858@qq.com>
2017-05-21 15:04:28 +0800

    make some change

--- study/wechatpay.php
+++ study/wechatpay.php
@@ -1,16 +1,16 @@
 <?php
-/**this is the procedure of wechat payment
-*OTDO: gonna show up the procedure of wechat payment
-**/
+	/**this is the procedure of wechat payment
+	*OTDO: gonna show up the procedure of wechat payment
+	**/
 class Peter 
 {
 	//stands for the configuration files
 	private $config;
-/**
-* TODO: fill up the config information 
-* HINT: you can search for helpful hint through wechat API document.
-* PARAM: $config stands for the configuration files, it's a php file.
-**/
+	/**
+	* TODO: fill up the config information 
+	* HINT: you can search for helpful hint through wechat API document.
+	* PARAM: $config stands for the configuration files, it's a php file.
+	**/
 	function stepOne_setConfig($config) 
 	{
 		if($this -> checkCode($config))	
@@ -23,21 +23,21 @@ class Peter
 		}
 		
 	};
-/**
-* TODO: check if the paramater is a file 
-* PARAM: $config file
-* RETURN: boolean true
-**/
+	/**
+	* TODO: check if the paramater is a file 
+	* PARAM: $config file
+	* RETURN: boolean true
+	**/
 	public function checkCode($config) 
 	{
 		if(is_file($config)) return true;
 	}
 
-/**
-* TODO: get access_token
-* @return: mixed | boolean | unknown
-* HINT: base on ThinkPHP framework
-**/
+	/**
+	* TODO: get access_token
+	* @return: mixed | boolean | unknown
+	* HINT: base on ThinkPHP framework
+	**/
 	public function getToken() 
 	{
 		//get cache, if access_token exists, return
@@ -64,11 +64,11 @@ class Peter
 		return $rseult['access_token'];
 	}
 
-/**
-* GET request
-* @param string $url
-*/
-	public function send_get($url) 
+	/**
+	* GET request
+	* @param string $url
+	*/
+	private function send_get($url) 
 	{
 		$oCurl = curl_init();
 		//check if its header is https://
@@ -98,5 +98,71 @@ class Peter
 			return false;
 		}
 		
+	}
+	/**
+	* TODO: send post request
+	* @param string $url
+	* @param array $param
+	* @param boolean $post_file (weather it's file uploading)
+	* @return string content
+	*/
+	private function send_post($url, $param, $post_file = false) 
+	{
+		$oCurl = curl_init();
+		//check if its header is started with https://
+		if(stripos($url, "https://") !== false) 
+		{
+			//set curl options
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+			curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+		}
+		//check if there is a file
+		if(is_string($param) || $post_file) 
+		{
+			$strPOST = $param;
+		} 
+		else 
+		{
+			$aPOST = array();
+			foreach($param as $key => $value) 
+			{
+				$aPOST[] = $key . "=" . urlencode($value);
+			}
+			//add & to the end of every element(except the last one)
+			$strPOST = join('&', $aPOST);
+		}
+		//curl continue
+		curl_setopt($oCurl, CURLOPT_URL, $url);
+		curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($oCurl, CURLOPT_POST, true);
+		curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+		$sContent = curl_exec($oCurl);
+		$aStatus = curl_getinfo($oCurl);
+		curl_close($oCurl);
+		if(intval($aStatus['http_code']) == 200) 
+		{
+			return $sContent;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+
+	/**
+	* TODO: send serve message
+	* $param array $data (format: {"touser":"OPENID", "msgtype":"news", "news":{.....}})
+	*/
+	public function sendCustomMessage($data) 
+	{
+		//get access_token
+		$token - $this -> getToken();
+		if(empty($token)) return false;
+		$url = "https://api.weixin.qq.com/alsdkjfladjf-sdalfhowe?access_token=%s";
+		$url = sprintf($url, $token);
+		//send post request to the url metioned above, 
+		$result = send_post($url, self::json_encode($data));
+
 	}
 }
