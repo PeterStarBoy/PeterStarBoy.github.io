6d4bad29dcf153150eb9840e2aeb41280329ff8d
PeterStarBoy <1574815858@qq.com>
2017-05-23 23:14:30 +0800

    add little tips

--- study/tips.php
+++ study/tips.php
@@ -3,3 +3,6 @@
 * INTRODUCTION: there are tips for better programming.
 */
 // 1. when you can't figure out the correct route, don't forget to look at the bottom address shown on your screen.(address column)
+/*
+	-----------------------ThinkPHP TIPS -----------------------------
+	1. when you use if tag in the htmlpage, please use $example['xxx'] instead of $example.xxx, ortherwise it will not be analysed
