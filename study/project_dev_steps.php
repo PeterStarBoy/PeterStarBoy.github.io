8bbb5a9b60a2075c3a38112cda79c543e2bed64c
PeterStarBoy <1574815858@qq.com>
2017-05-30 20:38:34 +0800

    project steps

---
+++ study/project_dev_steps.php
@@ -0,0 +1,13 @@
+<?php
+/*
+--------------THIS IS NROMAL STEPS WHEN START A PROJECT-----------
+			(1). NEEDS ANALYSIS, LIST ALL THE NEEDS THE PROJECT WANTED.
+			this info usually get from clients, so it's a literal needs.
+			(2). CONVERT ALL THESE NEEDS TO ACTUAL READABLE CODE NEEDS.
+			this usually get from product manager, pass the needs to coders.
+			(3). DRAW PROJECT FUNCTION STRUCTURE MAP.
+			need to draw UML structure map of project.
+			(4). DRAW SYSTEM FLOW CHART
+			(5). DATABASE DESIGNATION, DRAW E-R MAP
+			(6). CREATE DATABASE AND TABLES
+			(7). EVERY MODULE DESIGNATION AND PROGRAMMING
