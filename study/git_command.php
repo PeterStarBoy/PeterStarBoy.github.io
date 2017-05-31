6bf581abb07bcc5f9e40dbf5e635ec09b059e997
PeterStarBoy <1574815858@qq.com>
2017-05-31 20:40:33 +0800

    new addition

---
+++ study/git_command.php
@@ -0,0 +1,20 @@
+<?php
+	/*PETER!!!YOU'RE THE BEST, KEEP ON IT, BE YOURSELF!!!
+	----------------------here we showing some git command---------------------
+	(1). COMMAND FOR COMMITING
+	A. git status (SHOW CURRENT FILE TRACE STATEMENT)
+	B. git add FILENAME(INCLUDING FULL FILEPATH ACCORDING TO .git FILE)
+	C. git commit -m "UPLOAD COMMENT"
+	D. git push origin master (PUSH TO MAIN REPOSITORY)
+	(2). COMMAND FOR GENERATING SSHKEY
+	A. ssh-keygen -t -rsa -C "EMAIL ADDRESS"
+	B. ssh-add ~/.ssh/NEW NAME(only read id_rsa by default)
+	   IF IT SAYS count not open a connection to your authentication agent
+	   TRY THIS: ssh-agent bash;  ssh-add ~/.ssh/NEW NAME
+	(3). COMMAND FOR SET REMOTE ADDRESS
+	A. git remote add NAME GITSITE (add)
+	B. git remote rm NAME (delete)
+	C. git remote rename OLD_NAME NEW_NAME
+	(4). COMMAND FOR GIT VIM EDITOR
+	A. get out from it, shift+;, then q! or qw!
+	B. same commands as linux
